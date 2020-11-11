df839436e1ffd58d42b7b9d241c3b8e82b70741f
Doyeon <doyeon311@gmail.com>
2020-11-12 02:21:56 +0900

    코드 수정

--- level1/sum_of_factors.py
+++ level1/sum_of_factors.py
@@ -3,10 +3,4 @@ def solution(n):
     answer = 0
     test = [num for num in range(1,n+1) if not (n%num)]
     answer = sum(test)
-<<<<<<< HEAD
-=======
-    # for num in test:
-    #     if not (n % num): answer += num
-    # print(answer)
->>>>>>> b047df88beb03f14db0f4469247c332b6c4ee792
     return answer
