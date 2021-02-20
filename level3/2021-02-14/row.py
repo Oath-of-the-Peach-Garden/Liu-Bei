f64cab42753ab5f1b3d35c006a8920e12d44a2b5
doyeon <doyeon311@gmail.com>
2021-02-20 16:59:39 +0900

    풀이: 프로그래머스 level3 줄서는 방법

--- level3/2021-02-14/row.py
+++ level3/2021-02-14/row.py
@@ -1,26 +1,45 @@
 # 줄 서는 방법
 # https://programmers.co.kr/learn/courses/30/lessons/12936
+from math import factorial
+
+
+# def solution(n, k):
+#     answer = []
+#     check = [0] * n
+
+#     temp = [0] * n
+
+#     def dfs(i):
+#         if i == n:
+#             # print(temp, check)
+#             # return temp
+#             print(temp)
+#             answer.append(temp)
+#             return temp
+
+#         for j in range(n):
+#             if check[j] == 0:
+#                 check[j] = 1
+#                 temp[i] = j+1
+#                 dfs(i+1)
+#                 check[j] = 0
+#     # cnt += 1
+#     answer.append(dfs(0))
+#     print(answer)
+#     return answer
+
+
+#  다시시도
+
+
 def solution(n, k):
     answer = []
-    check = [0] * n
+    numbers = list(range(1, n+1))
+    # print(numbers)
 
-    temp = [0] * n
-
-    def dfs(i):
-        if i == n:
-            # print(temp, check)
-            # return temp
-            print(temp)
-            answer.append(temp)
-            return temp
-
-        for j in range(n):
-            if check[j] == 0:
-                check[j] = 1
-                temp[i] = j+1
-                dfs(i+1)
-                check[j] = 0
-    # cnt += 1
-    answer.append(dfs(0))
-    print(answer)
+    while n != 0:
+        fac = factorial(n-1)
+        answer.append(numbers.pop((k-1) // fac))
+        n -= 1
+        k %= fac
     return answer
