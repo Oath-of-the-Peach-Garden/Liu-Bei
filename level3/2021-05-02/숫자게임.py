9fcba4c7033125a27621066771072ac90c55f4b4
doyeon <doyeon311@gmail.com>
2021-05-01 22:52:56 +0900

    풀이: 프로그래머스 level3 숫자 게임

---
+++ level3/2021-05-02/숫자게임.py
@@ -0,0 +1,26 @@
+# 숫자 게임
+# https://programmers.co.kr/learn/courses/30/lessons/12987
+from collections import deque
+
+
+def solution(A, B):
+    answer = 0
+    A.sort()
+    B.sort()
+    A = deque(A)
+    B = deque(B)
+
+    a = A.pop()
+    b = B.pop()
+    while A:
+        # print(a, b)
+        # print(A, B)
+        if a < b:
+            answer += 1
+            b = B.pop()
+        a = A.pop()
+        # print("==========")
+    else:
+        if a < b:
+            answer += 1
+    return answer
