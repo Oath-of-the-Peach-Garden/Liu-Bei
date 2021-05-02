91ec8afd75638e7c72cb124552a0c8e2e7741dad
doyeon <doyeon311@gmail.com>
2021-05-02 22:23:20 +0900

    정리: 기지국설치 해설

--- level3/2021-05-02/기시국설치.py
+++ level3/2021-05-02/기시국설치.py
@@ -8,14 +8,21 @@ def solution(n, stations, w):
 
     area = 2 * w + 1
     start = 1
+
+    # 이미 설치된 기지국 s
     for s in stations:
+        # s에서 전파가 닿는 w만큼을 빼고 거기서 전파가 닿지 않는 제일 먼 곳 start를 빼줌
         aparts = s - w - start
+        # aparts가 1이상일 경우에 이 영역에 필요한 최소 기지국을 계산
         if aparts > 0:
             answer += ceil(aparts / area)
+        # 시작은 기지국의 오른쪽 끝으로 변경
         start = s + w + 1
     # print(answer, start, area)
+    # 갱신된 start가 n 이하일 경우 아직 커버해야하는 기지국이 남은 것
     if start <= n:
         # print(n+1-start / area)
+        # 최소 기지국 수를 한번 더 계산
         answer += ceil((n+1-start) / area)
 
     return answer
